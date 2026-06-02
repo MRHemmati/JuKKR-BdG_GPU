1c2be14fb322897c92f28bd01f53078270c79baa
Mohammad Hemmati <52596603+MRHemmati@users.noreply.github.com>
2026-06-02 15:31:17 +0200

    kloopz are fixed

--- source/KKRhost/dlke0.f90
+++ source/KKRhost/dlke0.f90
@@ -58,54 +58,9 @@ contains
 
     !$acc parallel loop collapse(3) copyin(kp, rr, ezoa, atom, cls, nacls, rcls, ginp) copy(gllke) private(ic, arg, tt, eikr, im, am)
     do i = 1, naez
-      do m = 1, nacls(cls(i))
+      do m = 1, naclsmax
         do lm2 = 1, lmgf0d
-          if (atom(m, i) >= 0) then
-            ic = cls(i)
-            if (use_deci_onebulk) then
-              arg(1) = -ci*tpi*rcls(1, m, ic)
-              arg(2) = -ci*tpi*rcls(2, m, ic)
-              arg(3) = -ci*tpi*rcls(3, m, ic)
-            else
-              arg(1) = -ci*tpi*rr(1, ezoa(m, i))
-              arg(2) = -ci*tpi*rr(2, ezoa(m, i))
-              arg(3) = -ci*tpi*rr(3, ezoa(m, i))
-            end if
-
-            tt = kp(1)*arg(1) + kp(2)*arg(2) + kp(3)*arg(3)
-            if (calc_complex_bandstructure) then
-              tt = tt + ci*(kp(4)*arg(1)+kp(5)*arg(2)+kp(6)*arg(3))
-            end if
-
-            eikr = exp(tt)*convpu
-
-            im = 1 + (m-1)*lmgf0d
-            am = 1 + (atom(m, i)-1)*lmgf0d
-            do lm1 = 1, lmgf0d
-              gllke(am + lm1 - 1, (i-1)*lmgf0d + lm2) = gllke(am + lm1 - 1, (i-1)*lmgf0d + lm2) + eikr * ginp(im + lm1 - 1, lm2, ic)
-            end do
-          end if
-        end do
-      end do
-    end do
-
-    if (symmetrize_gmat) then
-      allocate(gllke1(almgf0, almgf0))
-      gllke1(:, :) = (0.0_dp, 0.0_dp)
-
-      kp(1) = -bzkp(1)
-      kp(2) = -bzkp(2)
-      kp(3) = -bzkp(3)
-      if (calc_complex_bandstructure) then
-        kp(4) = -bzkp(4)
-        kp(5) = -bzkp(5)
-        kp(6) = -bzkp(6)
-      end if
-
-      !$acc parallel loop collapse(3) copyin(kp, rr, ezoa, atom, cls, nacls, rcls, ginp) copy(gllke1) private(ic, arg, tt, eikr, im, am)
-      do i = 1, naez
-        do m = 1, nacls(cls(i))
-          do lm2 = 1, lmgf0d
+          if (m <= nacls(cls(i))) then
             if (atom(m, i) >= 0) then
               ic = cls(i)
               if (use_deci_onebulk) then
@@ -128,8 +83,57 @@ contains
               im = 1 + (m-1)*lmgf0d
               am = 1 + (atom(m, i)-1)*lmgf0d
               do lm1 = 1, lmgf0d
-                gllke1(am + lm1 - 1, (i-1)*lmgf0d + lm2) = gllke1(am + lm1 - 1, (i-1)*lmgf0d + lm2) + eikr * ginp(im + lm1 - 1, lm2, ic)
+                gllke(am + lm1 - 1, (i-1)*lmgf0d + lm2) = gllke(am + lm1 - 1, (i-1)*lmgf0d + lm2) + eikr * ginp(im + lm1 - 1, lm2, ic)
               end do
+            end if
+          end if
+        end do
+      end do
+    end do
+
+    if (symmetrize_gmat) then
+      allocate(gllke1(almgf0, almgf0))
+      gllke1(:, :) = (0.0_dp, 0.0_dp)
+
+      kp(1) = -bzkp(1)
+      kp(2) = -bzkp(2)
+      kp(3) = -bzkp(3)
+      if (calc_complex_bandstructure) then
+        kp(4) = -bzkp(4)
+        kp(5) = -bzkp(5)
+        kp(6) = -bzkp(6)
+      end if
+
+      !$acc parallel loop collapse(3) copyin(kp, rr, ezoa, atom, cls, nacls, rcls, ginp) copy(gllke1) private(ic, arg, tt, eikr, im, am)
+      do i = 1, naez
+        do m = 1, naclsmax
+          do lm2 = 1, lmgf0d
+            if (m <= nacls(cls(i))) then
+              if (atom(m, i) >= 0) then
+                ic = cls(i)
+                if (use_deci_onebulk) then
+                  arg(1) = -ci*tpi*rcls(1, m, ic)
+                  arg(2) = -ci*tpi*rcls(2, m, ic)
+                  arg(3) = -ci*tpi*rcls(3, m, ic)
+                else
+                  arg(1) = -ci*tpi*rr(1, ezoa(m, i))
+                  arg(2) = -ci*tpi*rr(2, ezoa(m, i))
+                  arg(3) = -ci*tpi*rr(3, ezoa(m, i))
+                end if
+
+                tt = kp(1)*arg(1) + kp(2)*arg(2) + kp(3)*arg(3)
+                if (calc_complex_bandstructure) then
+                  tt = tt + ci*(kp(4)*arg(1)+kp(5)*arg(2)+kp(6)*arg(3))
+                end if
+
+                eikr = exp(tt)*convpu
+
+                im = 1 + (m-1)*lmgf0d
+                am = 1 + (atom(m, i)-1)*lmgf0d
+                do lm1 = 1, lmgf0d
+                  gllke1(am + lm1 - 1, (i-1)*lmgf0d + lm2) = gllke1(am + lm1 - 1, (i-1)*lmgf0d + lm2) + eikr * ginp(im + lm1 - 1, lm2, ic)
+                end do
+              end if
             end if
           end do
         end do
